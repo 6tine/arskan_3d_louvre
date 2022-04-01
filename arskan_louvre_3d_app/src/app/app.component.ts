6004a8711fb685b01acf24d236a451fe637cb47a
traverss <sixtine.traverse@edu.esiee.fr>
2022-04-01 13:06:18 +0200

    modif css pas ouf pas fini

--- arskan_louvre_3d_app/src/app/app.component.ts
+++ arskan_louvre_3d_app/src/app/app.component.ts
@@ -9,40 +9,4 @@ import {ApiServiceService} from "./services/api-service.service";
 export class AppComponent {
   title = 'arskan_louvre_3d_app';
 
-  constructor(public apiService: ApiServiceService) {
-
-  }
-
-  test(){
-    this.apiService.getAllPointersFrom("62278d4dcd78795d5ce9b52b")
-      .subscribe(
-        resp => {
-          console.log(resp)
-        }
-      )
-  }
-
-  test2() {
-    let name = 'toto';
-    let profileId;
-    this.apiService.getProfiles().subscribe(
-      profiles => {
-        console.log(profiles)
-        profileId = profiles[2]['id']
-        console.log('profileId : ', profileId)
-        this.apiService.getObjectEmbedUrl("62278d4dcd78795d5ce9b52b",profileId, name)
-          .subscribe(
-            resp => {
-              console.log(resp)
-            }
-          )
-      }
-    )
-    /*this.apiService.getOneObjectEmbed("62278d4dcd78795d5ce9b52b")
-      .subscribe(
-        resp => {
-          console.log(resp)
-        }
-      )*/
-  }
 }
